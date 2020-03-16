2b9e369c3fa1c9ee2ae151adc137eb1ffee364f8
Jonas Bernoulli <jonas@bernoul.li>
2020-03-17 00:17:55 +0100

    [maybe] define-ffi-library: Add optional filename argument

--- ffi.el
+++ ffi.el
@@ -45,12 +45,13 @@ is specified, in which case search that list of directories instead."
 
 (gv-define-simple-setter ffi--mem-ref ffi--mem-set t)
 
-(defmacro define-ffi-library (symbol name)
+(defmacro define-ffi-library (symbol name &optional filename)
   `(progn
      (defvar ,symbol nil)
      (defun ,symbol ()
        (or ,symbol
-           (setq ,symbol (ffi--dlopen (ffi--locate-module ,name)))))))
+           (setq ,symbol
+                 (ffi--dlopen (or ,filename (ffi--locate-module ,name))))))))
 
 (defmacro define-ffi-function (name c-name return-type arg-types library)
   (declare (indent defun))
