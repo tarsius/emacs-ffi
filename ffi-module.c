e13df774da0c03d67acc90b07ab419fa6f3bbb79
Jonas Bernoulli <jonas@bernoul.li>
2024-11-09 14:32:33 +0100

    module: Include config.h
    Without this we get many errors like this:
    
    In file included from /.../emacs-module.h:29,
                     from ffi-module.c:16:
    /.../lib/stddef.h:128:3: error: #error "Please include config.h first."
      128 |  #error "Please include config.h first."
          |   ^~~~~
    
    All I know about this is that the compiler told me to do it and after
    I did so, it stopped complaining.

--- ffi-module.c
+++ ffi-module.c
@@ -13,6 +13,7 @@ GNU General Public License for more details.
 You should have received a copy of the GNU General Public License
 along with this.  If not, see <https://www.gnu.org/licenses/>.  */
 
+#include <config.h>
 #include "emacs-module.h"
 
 #include <ffi.h>
