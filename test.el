1133abbfc5d5884fbfc5a3e434a629eab6b82b55
Jonas Bernoulli <jonas@bernoul.li>
2025-04-03 15:35:16 +0200

    test.el: Silence byte-compiler

--- test.el
+++ test.el
@@ -5,16 +5,16 @@
 (require 'ert)
 (require 'ffi)
 
-(define-ffi-library test.so "test")
+(define-ffi-library test-lib "test")
 
-(define-ffi-function test-function "test_function" :int nil test.so)
-(define-ffi-function test-function-char "test_function" :char nil test.so)
+(define-ffi-function test-function "test_function" :int nil test-lib)
+(define-ffi-function test-function-char "test_function" :char nil test-lib)
 
 (ert-deftest ffi-basic ()
   (should (= (test-function) 27))
   (should (= (test-function-char) 27)))
 
-(define-ffi-function test-c-string "test_c_string" :pointer nil test.so)
+(define-ffi-function test-c-string "test_c_string" :pointer nil test-lib)
 
 (ert-deftest ffi-pointer-type ()
   (should (eq (type-of (test-c-string)) 'user-ptr)))
@@ -22,18 +22,18 @@
 (ert-deftest ffi-c-string ()
   (should (equal (ffi-get-c-string (test-c-string)) "hello")))
 
-(defun callback (arg)
+(defun test-callback (arg)
   (1+ arg))
 
 (define-ffi-function test-call-callback "test_call_callback"
-  :int [:pointer] test.so)
+  :int [:pointer] test-lib)
 
 (ert-deftest ffi-call-callback ()
   (let* ((cif (ffi--prep-cif :int [:int]))
-         (pointer-to-callback (ffi-make-closure cif #'callback)))
+         (pointer-to-callback (ffi-make-closure cif #'test-callback)))
     (should (eq (test-call-callback pointer-to-callback) 23))))
 
-(define-ffi-function test-add "test_add" :int [:int :int] test.so)
+(define-ffi-function test-add "test_add" :int [:int :int] test-lib)
 
 (ert-deftest ffi-add ()
   (should (eq (test-add 23 -23) 0)))
@@ -62,7 +62,7 @@
   (intval :type :int))
 
 (define-ffi-function test-get-struct "test_get_struct"
-  test-struct nil test.so)
+  test-struct nil test-lib)
 
 (ert-deftest ffi-structure-return ()
   (let ((struct-value (test-get-struct)))
@@ -71,7 +71,7 @@
     (should (eq (test-struct-intval struct-value) 23))))
 
 (define-ffi-function test-get-struct-int "test_get_struct_int"
-  :int (test-struct) test.so)
+  :int (test-struct) test-lib)
 
 (ert-deftest ffi-struct-modification ()
   (let ((struct-value (test-get-struct)))
@@ -84,7 +84,7 @@
   (ival :type :int))
 
 (define-ffi-function test-get-union "test_get_union"
-  test-union nil test.so)
+  test-union nil test-lib)
 
 (ert-deftest ffi-union ()
   (let ((object (test-get-union)))
@@ -94,7 +94,7 @@
 (ert-deftest ffi-null ()
   (should (ffi-pointer-null-p (ffi-null-pointer))))
 
-(define-ffi-function test-not "test_not" :bool (:bool) test.so)
+(define-ffi-function test-not "test_not" :bool (:bool) test-lib)
 
 (ert-deftest ffi-boolean ()
   (should (eq (test-not nil) t))
@@ -105,8 +105,10 @@
 (defconst test-user-defined-char :char)
 
 (ert-deftest ffi-array ()
-  (should (eq (define-ffi-array arr1 :char 1024) 'arr1))
-  (should (eq (define-ffi-array arr2 test-user-defined-char 1024) 'arr2)))
+  (should (eq (define-ffi-array test-arr1 :char 1024)
+              'test-arr1))
+  (should (eq (define-ffi-array test-arr2 test-user-defined-char 1024)
+              'test-arr2)))
 
 (ert-deftest ffi-with-ffi-multi-stat ()
   (should (eq (with-ffi-temporary (a :int) 1 2 3) 3))
